69578b6a89515b32a12b0c471f29fa44fb09e732
tegarrramadhaaan <anakrama2000@gmail.com>
2025-06-10 10:12:49 +0700

    Menulis dan Membaca File Teks di C++

---
+++ iofile.cpp
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+    string baris;
+    
+    ofstream outfile;
+    outfile.open("contohfile.txt");
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    while (true)
+    {
+    cout << "- ";
+        getline(cin, baris);
+        if(baris == "q")
+        break;
+        
+        outfile << baris << endl;
+    }
+
+outfile.close();
+
+}
