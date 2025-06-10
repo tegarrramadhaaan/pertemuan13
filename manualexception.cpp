77770e07afe5a5fd6e489691fb91377fcb91f39d
tegarrramadhaaan <anakrama2000@gmail.com>
2025-06-10 10:51:46 +0700

    Menambahkan Program C++: Penanganan Eksepsi dengan Catch Default

---
+++ manualexception.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try
+    {
+        cout << "selamat belajar di prodi TI UMY" << endl;
+        throw 5 ;
+        cout << "pernyataan tidak akan dieksekusi " << endl;
+    }
+   
+    
+}
